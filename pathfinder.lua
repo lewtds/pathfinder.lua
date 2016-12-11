7de692dbaf79435848ae829814252be2385ca821
Trung Ngo <ndtrung4419@gmail.com>
2016-12-11 15:27:02 +0200

    Convert indentation to 2 spaces

--- pathfinder.lua
+++ pathfinder.lua
@@ -1,8 +1,8 @@
 local function table_get_default(table, key, default)
-	if not table[key] then
-		table[key] = default
-	end
-	return table[key]
+  if not table[key] then
+    table[key] = default
+  end
+  return table[key]
 end
 
 
@@ -10,18 +10,18 @@ end
 local PriorityQueue = {}
 
 function PriorityQueue.new(compare_nodes)
-	return {
-		compare_nodes = compare_nodes
-	}
+  return {
+    compare_nodes = compare_nodes
+  }
 end
 
 function PriorityQueue.add(queue, node)
-	table.insert(queue, node)
-	table.sort(queue, queue.compare_nodes)
+  table.insert(queue, node)
+  table.sort(queue, queue.compare_nodes)
 end
 
 function PriorityQueue.pop(queue)
-	return table.remove(queue)
+  return table.remove(queue)
 end
 
 
@@ -30,95 +30,95 @@ end
 local Pathfinder = {}
 
 local function constant_weight(n1, n2)
-	return 1
+  return 1
 end
 
 -- neighbors:node => List<{node, weight}>
 -- weight:node => node => number
 -- heuristic:node => number
 function Pathfinder.astar(start, dest, neighbors, weight, heuristic)
-	weight = weight or constant_weight
-	local come_from = {}
-	local fscore = {[start] = 0}
-	local gscore = {[start] = 0}
+  weight = weight or constant_weight
+  local come_from = {}
+  local fscore = {[start] = 0}
+  local gscore = {[start] = 0}
 
-	local to_search = PriorityQueue.new(function (n1, n2)
-		return table_get_default(fscore, n1, math.huge) > table_get_default(fscore, n2, math.huge)
-	end)
+  local to_search = PriorityQueue.new(function (n1, n2)
+    return table_get_default(fscore, n1, math.huge) > table_get_default(fscore, n2, math.huge)
+  end)
 
-	local prev = nil
-	local current = start
-	while current and current ~= dest do
-		for i,node in ipairs(neighbors(current)) do
-			if not come_from[node] then
-				local tentative_gscore = table_get_default(gscore, current, math.huge) + weight(current, node)
+  local prev = nil
+  local current = start
+  while current and current ~= dest do
+    for i,node in ipairs(neighbors(current)) do
+      if not come_from[node] then
+        local tentative_gscore = table_get_default(gscore, current, math.huge) + weight(current, node)
 
-				if gscore[node] == nil then
-					PriorityQueue.add(to_search, node)
-					
-					if tentative_gscore < table_get_default(gscore, node, math.huge) then
-						come_from[node] = current
-						gscore[node] = tentative_gscore
-						fscore[node] = tentative_gscore + heuristic(node, dest)
-					end
-				end
-			end
-		end
+        if gscore[node] == nil then
+          PriorityQueue.add(to_search, node)
 
-		prev = current
-		current = PriorityQueue.pop(to_search)
-	end
+          if tentative_gscore < table_get_default(gscore, node, math.huge) then
+            come_from[node] = current
+            gscore[node] = tentative_gscore
+            fscore[node] = tentative_gscore + heuristic(node, dest)
+          end
+        end
+      end
+    end
 
-	return come_from
+    prev = current
+    current = PriorityQueue.pop(to_search)
+  end
+
+  return come_from
 end
 
 
 function Pathfinder.shortest_path(arg)
-	local start, dest, neighbors, weight, heuristic = arg.start, arg.dest, arg.neighbors, arg.weight, arg.heuristic
-	local come_from = Pathfinder.astar(start, dest, neighbors, weight, heuristic)
+  local start, dest, neighbors, weight, heuristic = arg.start, arg.dest, arg.neighbors, arg.weight, arg.heuristic
+  local come_from = Pathfinder.astar(start, dest, neighbors, weight, heuristic)
 
-	local path = {}
-	local current = dest
-	while true do
-		local prev = come_from[current]
-		if prev == nil then break end
+  local path = {}
+  local current = dest
+  while true do
+    local prev = come_from[current]
+    if prev == nil then break end
 
-		table.insert(path, 1, current)
-		current = prev
-	end
+    table.insert(path, 1, current)
+    current = prev
+  end
 
-	return path
+  return path
 end
 
 
 
 function Pathfinder.weights_from_point(arg)
-	local start, neighbors, weight, max_weight = 
-		arg.start, 
-		arg.neighbors, 
-		arg.weight or constant_weight, 
-		arg.max_weight or math.huge
+  local start, neighbors, weight, max_weight =
+    arg.start,
+    arg.neighbors,
+    arg.weight or constant_weight,
+    arg.max_weight or math.huge
 
-	local weights = {[start] = 0}
-	local to_search = {}
+  local weights = {[start] = 0}
+  local to_search = {}
 
-	local current = start
-	while current do
-		if weights[current] >= max_weight then
-			break
-		end
+  local current = start
+  while current do
+    if weights[current] >= max_weight then
+      break
+    end
 
-		for _,node in ipairs(neighbors(current)) do
-			if not weights[node] then
-				weights[node] = weights[current] + weight(current, node)
-				table.insert(to_search, 1, node)
-			end
-		end
+    for _,node in ipairs(neighbors(current)) do
+      if not weights[node] then
+        weights[node] = weights[current] + weight(current, node)
+        table.insert(to_search, 1, node)
+      end
+    end
 
-		current = table.remove(to_search)
-	end
+    current = table.remove(to_search)
+  end
 
-	return weights
+  return weights
 end
 
 
